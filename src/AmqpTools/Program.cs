77193caebb3387bf22659587eb3ab2206f47587b
Erik <subreality68@gmail.com>
2024-07-12 12:21:30 -0600

    comment out

--- src/AmqpTools/Program.cs
+++ src/AmqpTools/Program.cs
@@ -5,20 +5,22 @@ using AmqpTools.Core.Commands.Peek;
 using AmqpTools.Core.Commands.Publish;
 using AmqpTools.Core.Commands.Queue;
 using AmqpTools.Core.Commands.Shovel;
-using CommandLine;
 using Microsoft.Extensions.Logging;
 
 namespace AmqpTools {
     public class Program {
         private static ILogger<Program> logger;
         public static int Main(string[] args) {
-            // testing spoof on trimming for AOT
-            PublishCommand publishCommand = new PublishCommand();
             Type[] types = new Type[] { typeof(PublishCommand), typeof(QueueCommand), typeof(PeekCommand), typeof(DeleteMessageCommand), typeof(ShovelCommand) };
-            var result = CommandLine.Parser.Default.ParseArguments(args, types);
-            result.WithParsed(x => {
-                Console.WriteLine($"parsed type result: {x}");
-            });
+            // either way requires binding options to command classes for the commandlineparser to successfully parse - but moot, since its parser is not compatable with trimming for  aot
+            //var result = CommandLine.Parser.Default.ParseArguments(args, types);
+            //var result = CommandLine.Parser.Default.ParseArguments<PublishCommand, QueueCommand, PeekCommand, DeleteMessageCommand, ShovelCommand>(args);
+            //result.WithParsed(x => {
+            //    Console.WriteLine($"parsed type result: {x}");
+            //})
+            //    .WithNotParsed(x => {
+            //        Console.WriteLine("command type not parsed");
+            //    });
             //.WithParsed(opts => Run(opts))
             //  .WithNotParsed<PublisherOptions>((errs) => HandleParseError(errs));
 
